c758dd77336b656e7f3c518614646839f318ccf8
behzoddev <behzoddev@gmail.com>
2023-05-27 17:46:57 +0900

    Initial commit

---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,20 @@
+object Version {
+    const val coreKtx  = ""
+}
+
+object Libs {
+    object Android {
+
+    }
+    object MlKit {
+
+    }
+
+    object Camerax {
+
+    }
+
+    object Picker {
+
+    }
+}
